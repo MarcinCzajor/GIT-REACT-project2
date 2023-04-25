400b17de0379f7465ed10536fd5d360d3681b6bb
Marcin <120125611+MarcinCzajor@users.noreply.github.com>
2023-04-25 17:01:49 +0200

    Update CurrencySelect.jsx

--- src/components/CurrencySelect.jsx
+++ src/components/CurrencySelect.jsx
@@ -1,13 +1,15 @@
-const OptionsChoice = ({onChange, selectedCurrency}) => {
-
-
-    return (
-<select className="selectCurr" value={selectedCurrency} onChange={onChange}>
-    <option value="EUR">EUR</option>
-    <option value="USD">USD</option>
-    <option value="CHF">CHF</option>
-</select>
-    )
+const OptionsChoice = ({ onChange, selectedCurrency }) => {
+	return (
+		<select
+			className="selectCurr"
+			value={selectedCurrency}
+			onChange={onChange}
+		>
+			<option value="EUR">EUR</option>
+			<option value="USD">USD</option>
+			<option value="CHF">CHF</option>
+		</select>
+	);
 };
 
 export default OptionsChoice;
